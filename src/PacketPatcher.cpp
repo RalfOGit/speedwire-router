c886772f766e29a8eb840b19e0c268897c59bd5b
RALFS-VIVOBOOK\Admin <ralfostermann@web.de>
2023-10-29 17:15:14 +0100

    cleaner represetation of tag headers, deprecated cross-layer getter and setter methods in class SpeedwireHeader

--- src/PacketPatcher.cpp
+++ src/PacketPatcher.cpp
@@ -23,40 +23,45 @@ PacketPatcher::PacketPatcher(void) {
 void PacketPatcher::patch(SpeedwireHeader& speedwire_packet, struct sockaddr& src) {
 #if 1
     // example implementation for an emeter packet patch
-    uint32_t group      = speedwire_packet.getGroup();
-    uint16_t length     = speedwire_packet.getLength();
-    uint16_t protocolID = speedwire_packet.getProtocolID();
-    int      offset     = speedwire_packet.getPayloadOffset();
 
-    // check if it is an emeter packet
-    if (speedwire_packet.isEmeterProtocolID() == true) {
-        SpeedwireEmeterProtocol emeter_packet(speedwire_packet);
-        uint16_t susyid = emeter_packet.getSusyID();
-        uint32_t serial = emeter_packet.getSerialNumber();
-        uint32_t timer  = emeter_packet.getTime();
+    // check if it is a valid data2 packet
+    if (speedwire_packet.isValidData2Packet()) {
+        SpeedwireData2Packet data2_packet(speedwire_packet);
+        uint16_t length     = data2_packet.getTagLength();
+        uint16_t protocolID = data2_packet.getProtocolID();
+        int      offset     = data2_packet.getPayloadOffset();
 
-        // loop across all obis data in the emeter packet
-        void* obis = emeter_packet.getFirstObisElement();
-        while (obis != NULL) {
-            //emeter_packet.printObisElement(obis, stderr);
-            const uint8_t  channel = emeter_packet.getObisChannel(obis);
-            const uint8_t  index   = emeter_packet.getObisIndex(obis);
-            const uint8_t  type    = emeter_packet.getObisType(obis);
-            const uint8_t  tariff  = emeter_packet.getObisTariff(obis);
-            const uint32_t value   = emeter_packet.getObisValue4(obis);
+        // check if it is an emeter packet
+        if (data2_packet.isEmeterProtocolID() == true) {
+            SpeedwireEmeterProtocol emeter_packet(data2_packet);
 
-            // limit the negative active total power to some given value
-            const ObisData& negActPowTot = ObisData::NegativeActivePowerTotal;
-            if (channel == negActPowTot.channel && index == negActPowTot.index && type == negActPowTot.type && tariff == negActPowTot.tariff) {
-                uint32_t max_value = emeter_packet.getObisValue4(maxNegativeActivePowerTotalBytes.data());
-                if (value > max_value) {
-                    fprintf(stdout, "limited max negative active power from %ul to %ul\n", value, max_value);
-                    emeter_packet.setObisElement(obis, maxNegativeActivePowerTotalBytes.data());
+            uint16_t susyid = emeter_packet.getSusyID();
+            uint32_t serial = emeter_packet.getSerialNumber();
+            uint32_t timer  = emeter_packet.getTime();
+
+            // loop across all obis data in the emeter packet
+            void* obis = emeter_packet.getFirstObisElement();
+            while (obis != NULL) {
+                //emeter_packet.printObisElement(obis, stderr);
+                const uint8_t  channel = emeter_packet.getObisChannel(obis);
+                const uint8_t  index   = emeter_packet.getObisIndex(obis);
+                const uint8_t  type    = emeter_packet.getObisType(obis);
+                const uint8_t  tariff  = emeter_packet.getObisTariff(obis);
+                const uint32_t value   = emeter_packet.getObisValue4(obis);
+
+                // limit the negative active total power to some given value
+                const ObisData& negActPowTot = ObisData::NegativeActivePowerTotal;
+                if (channel == negActPowTot.channel && index == negActPowTot.index && type == negActPowTot.type && tariff == negActPowTot.tariff) {
+                    uint32_t max_value = emeter_packet.getObisValue4(maxNegativeActivePowerTotalBytes.data());
+                    if (value > max_value) {
+                        fprintf(stdout, "limited max negative active power from %ul to %ul\n", value, max_value);
+                        emeter_packet.setObisElement(obis, maxNegativeActivePowerTotalBytes.data());
+                    }
                 }
-            }
 
-            // proceed to next obis element
-            obis = emeter_packet.getNextObisElement(obis);
+                // proceed to next obis element
+                obis = emeter_packet.getNextObisElement(obis);
+            }
         }
     }
 #endif
--- src/SpeedwirePacketReceiver.cpp
+++ src/SpeedwirePacketReceiver.cpp
@@ -21,45 +21,49 @@ EmeterPacketReceiver::EmeterPacketReceiver(LocalHost& host, std::vector<Speedwir
     senders(sender),
     bounceDetector(),
     packetPatcher() {
-    protocolID = SpeedwireHeader::sma_emeter_protocol_id;
+    protocolID = SpeedwireData2Packet::sma_emeter_protocol_id;
 }
 
 /**
  *  Receive method - can be called with arbitrary speedwire packets
  */
 void EmeterPacketReceiver::receive(SpeedwireHeader& speedwire_packet, struct sockaddr& src) {
-    uint32_t group      = speedwire_packet.getGroup();
-    uint16_t length     = speedwire_packet.getLength();
-    uint16_t protocolID = speedwire_packet.getProtocolID();
-    int      offset     = speedwire_packet.getPayloadOffset();
 
-    // check if it is an emeter packet
-    if (speedwire_packet.isEmeterProtocolID()         == true ||
-        speedwire_packet.isExtendedEmeterProtocolID() == true) {
-        SpeedwireEmeterProtocol emeter_packet(speedwire_packet);
-        uint16_t susyid = emeter_packet.getSusyID();
-        uint32_t serial = emeter_packet.getSerialNumber();
-        uint32_t timer = emeter_packet.getTime();
+    // check if it is a valid data2 packet
+    if (speedwire_packet.isValidData2Packet()) {
+        const SpeedwireData2Packet data2_packet(speedwire_packet);
+        uint16_t length = data2_packet.getTagLength();
+        uint16_t protocolID = data2_packet.getProtocolID();
+        int      offset = data2_packet.getPayloadOffset();
 
-        // perform some simple multicast bounce back prevention
-        if (bounceDetector.isBouncedPacket(emeter_packet, src) == true) {
-            logger.print(LogLevel::LOG_INFO_1, "received bounced emeter packet from %s susyid %u serial %lu time %lu\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
-            return;
-        }
-        bounceDetector.receive(emeter_packet, src);
-        logger.print(LogLevel::LOG_INFO_1, "received emeter packet from %s susyid %u serial %lu time %lu\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
+        // check if it is an emeter packet
+        if (data2_packet.isEmeterProtocolID() == true) {
+            const SpeedwireEmeterProtocol emeter_packet(data2_packet);
 
-        // patch packet if required
-        packetPatcher.patch(speedwire_packet, src);
+            uint16_t susyid = emeter_packet.getSusyID();
+            uint32_t serial = emeter_packet.getSerialNumber();
+            uint32_t timer = emeter_packet.getTime();
 
-        // loop across obis data in the emeter packet
-        //for (void* obis = emeter_packet.getFirstObisElement(); obis != NULL; obis = emeter_packet.getNextObisElement(obis)) {
-        //    obis = emeter_packet.getNextObisElement(obis);
-        //}
+            // perform some simple multicast bounce back prevention
+            if (bounceDetector.isBouncedPacket(emeter_packet, src) == true) {
+                logger.print(LogLevel::LOG_INFO_1, "received bounced emeter packet from %s susyid %u serial %lu time %lu\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
+                return;
+            }
+            bounceDetector.receive(emeter_packet, src);
+            logger.print(LogLevel::LOG_INFO_1, "received emeter packet from %s susyid %u serial %lu time %lu\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
 
-        // forward the emeter packet to all registered producers
-        for (auto& sender : senders) {
-            sender->send(speedwire_packet, src);
+            // patch packet if required
+            packetPatcher.patch(speedwire_packet, src);
+
+            // loop across obis data in the emeter packet
+            //for (void* obis = emeter_packet.getFirstObisElement(); obis != NULL; obis = emeter_packet.getNextObisElement(obis)) {
+            //    obis = emeter_packet.getNextObisElement(obis);
+            //}
+
+            // forward the emeter packet to all registered producers
+            for (auto& sender : senders) {
+                sender->send(speedwire_packet, src);
+            }
         }
     }
 }
@@ -73,38 +77,44 @@ InverterPacketReceiver::InverterPacketReceiver(LocalHost& host, std::vector<Spee
     senders(sender),
     bounceDetector(),
     packetPatcher() {
-    protocolID = SpeedwireHeader::sma_inverter_protocol_id; 
+    protocolID = SpeedwireData2Packet::sma_inverter_protocol_id;
 }
 
 /**
  *  Receive method - can be called with arbitrary speedwire packets
  */
 void InverterPacketReceiver::receive(SpeedwireHeader& speedwire_packet, struct sockaddr& src) {
-    uint32_t group      = speedwire_packet.getGroup();
-    uint16_t length     = speedwire_packet.getLength();
-    uint16_t protocolID = speedwire_packet.getProtocolID();
-    int      offset     = speedwire_packet.getPayloadOffset();
 
-    if (speedwire_packet.isInverterProtocolID() == true) {
-        SpeedwireInverterProtocol inverter_packet(speedwire_packet);
-        uint16_t susyid = inverter_packet.getSrcSusyID();
-        uint32_t serial = inverter_packet.getSrcSerialNumber();
-        uint32_t timer  = (uint32_t)LocalHost::getUnixEpochTimeInMs();
+    // check if it is a valid data2 packet
+    if (speedwire_packet.isValidData2Packet()) {
+        const SpeedwireData2Packet data2_packet(speedwire_packet);
+        uint16_t length = data2_packet.getTagLength();
+        uint16_t protocolID = data2_packet.getProtocolID();
+        int      offset = data2_packet.getPayloadOffset();
 
-        // perform some simple multicast bounce back prevention
-        if (bounceDetector.isBouncedPacket(inverter_packet, src) == true) {
-            logger.print(LogLevel::LOG_INFO_1, "received bounced inverter packet from %s susyid %u serial %lu time %lu => DROPPED\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
-            return;
-        }
-        bounceDetector.receive(inverter_packet, src);
-        logger.print(LogLevel::LOG_INFO_1, "received inverter packet from %s susyid %u serial %lu time %lu => DROPPED\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
+        // check if it is an inverter packet
+        if (data2_packet.isInverterProtocolID() == true) {
+            const SpeedwireInverterProtocol inverter_packet(speedwire_packet);
 
-        // patch packet if required
-        packetPatcher.patch(speedwire_packet, src);
+            uint16_t susyid = inverter_packet.getSrcSusyID();
+            uint32_t serial = inverter_packet.getSrcSerialNumber();
+            uint32_t timer = (uint32_t)LocalHost::getUnixEpochTimeInMs();
 
-        // forward the emeter packet to all registered producers
-        for (auto& sender : senders) {
-            sender->send(speedwire_packet, src);
+            // perform some simple multicast bounce back prevention
+            if (bounceDetector.isBouncedPacket(inverter_packet, src) == true) {
+                logger.print(LogLevel::LOG_INFO_1, "received bounced inverter packet from %s susyid %u serial %lu time %lu => DROPPED\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
+                return;
+            }
+            bounceDetector.receive(inverter_packet, src);
+            logger.print(LogLevel::LOG_INFO_1, "received inverter packet from %s susyid %u serial %lu time %lu => DROPPED\n", AddressConversion::toString(src).c_str(), susyid, serial, timer);
+
+            // patch packet if required
+            packetPatcher.patch(speedwire_packet, src);
+
+            // forward the emeter packet to all registered producers
+            for (auto& sender : senders) {
+                sender->send(speedwire_packet, src);
+            }
         }
     }
 }
